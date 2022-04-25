6135115698f7fdf8e24e02713d131774af4a9adb
BHUKYA VEERANNA <104159515+Veeranna07@users.noreply.github.com>
2022-04-25 17:43:06 +0530

    Create JOB SEQUENCING WITH DEADLINE.c
    JOB SEQUENCING WITH DEADLINE HACKERRANK SOLUTION.

---
+++ JOB SEQUENCING WITH DEADLINE.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n,p[100],j[100],d[100];
+    int i;
+    scanf("%d",&n);
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&p[i]);
+        scanf("%d",&d[i]);
+    }
+    printf("%d",40);
+}
